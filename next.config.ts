83b325c2428157db858cb297ab6c0ee8e337ce70
akshukisku <akshay.kisku4@gmail.com>
2026-07-02 13:51:35 +0530

    PWA setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
